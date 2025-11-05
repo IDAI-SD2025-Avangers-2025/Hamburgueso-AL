ae11e7da9008e1d9d3ebc6955e2ce5dc52626d5d
AngelL218 <al45850@un.edu.mx>
2025-11-05 09:36:55 -0600

    nuevas funciones 2

--- calculos.js
+++ calculos.js
@@ -2,6 +2,10 @@ var formulario = document.getElementById("calcularPrecio");
 
 function calcular() {
 
+    var tamano = formulario.tam.value;
+    var pan = formulario.pan.value;
+    var combo = formulario.combo.value;
+
     //alert("si funciona mi boton")
 
     var cantidad = document.getElementById("cantidad").value;
@@ -16,45 +20,47 @@ function calcular() {
         //var tamañoGrande = document.getElementById("grande").value;
     //alert(tamañoGrande);
 
-    var tamaño = formulario.tam.value;
-    
-        var pan = formulario.pan.value;
-
-            var combo = formulario.combo.value;
-
-
 
     var total = 0;
 
-    if(tamaño == "chico") {
+    if(tamano == "chico") {
         total = 100;    }
 
-        if(tamaño == "mediano") {
-            total = 150;      }
+    else if(tamano == "mediano") {
+        total = 150;      }
 
-            if(tamaño == "grande") {
-                total = 200;     }
+        
+    else if(tamano == "grande") {
+            total = 200;     }
 
-
-
-                if(formulario.pan.value == "integral") {
+    else{
+                alert("Por favor selecciona un tamaño");
+                return;
+            }
+    
+    
+    if(pan == "integral") {
                 total = total + 0;     }
 
-                    if(formulario.pan.value == "blanco") {
+                   else if(pan == "blanco") {
                     total = total + 5;     }
 
-                        if(formulario.pan.value == "papa") {
+                       else if(pan == "papa") {
                         total = total + 5;     }
 
-                            if(formulario.pan.value == "parmesano") {
+                          else if(pan == "parmesano") {
                             total = total + 5;     }
+
+
+                                    else{
+                                        alert("Por favor selecciona un tipo de pan");
+                                        return;
+                                    }
 
 
                                 if(formulario.combo.value == "si") {
                                 total = total + 75;     }  
                                 
-                                    if(formulario.combo.value == "no") {
-                                    total = total;   }
 
 
 
